9fcaa50a02583dd91b262e5be39cfac3110ac732
jkvim <jkvim@outlook.com>
2019-07-27 22:49:49 +0800

    set http interceptor as multi

--- src/app/interceptors/index.ts
+++ src/app/interceptors/index.ts
@@ -3,6 +3,6 @@ import { HttpErrorInterceptor } from './http-error.interceptor';
 import { NoopIntercetor } from './noop-interceptor';
 
 export const httpInterceptorProviders = [
-  { provide: HTTP_INTERCEPTORS, useClass: NoopIntercetor },
-  { provide: HTTP_INTERCEPTORS, useClass: HttpErrorInterceptor }
+  { provide: HTTP_INTERCEPTORS, useClass: NoopIntercetor, multi: true },
+  { provide: HTTP_INTERCEPTORS, useClass: HttpErrorInterceptor, multi: true }
 ];
